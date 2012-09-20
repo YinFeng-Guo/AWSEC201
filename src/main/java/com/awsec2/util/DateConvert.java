ef580af1446004619645365ce6875a697f1a422b
BinYuan <bin.yuan@itbconsult.com>
2012-09-20 10:03:34 +0800

    Bad date format correction

--- src/main/java/com/awsec2/util/DateConvert.java
+++ src/main/java/com/awsec2/util/DateConvert.java
@@ -14,8 +14,14 @@ public class DateConvert extends DefaultTypeConverter {
 			// 因为在Struts2里会表单传过来的非字符串数据转换为String[],所以这里得取第一个值
 			String dataStr = ((String[]) value)[0];
 			SimpleDateFormat f = new SimpleDateFormat("yyyy-MM-dd"); // 转换为自己想要日期格式
+			String dateStr[] = dataStr.split("-");
+			for(int i = 0 ; i<dateStr.length; i++) {
+				if(dateStr[i].length()==0) {
+					return null;
+				}
+			}
 			try {
-				if(dataStr == "") {
+				if(dataStr == "" || dateStr.length !=3 ) {
 					return null;
 				}
 				return f.parse(dataStr);
